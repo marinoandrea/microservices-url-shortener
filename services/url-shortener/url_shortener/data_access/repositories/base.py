163ab3776c7901f7db223c3ca6b4024df09fccfa
Andrea Marino <am.marinoandrea@gmail.com>
2022-04-04 14:05:26 +0200

    Defined repositories interfaces

---
+++ services/url-shortener/url_shortener/data_access/repositories/base.py
@@ -0,0 +1,26 @@
+
+
+from abc import ABC, abstractmethod
+from typing import Generic, Optional, TypeVar
+
+from url_shortener.entities.shortened_url import ShortenedURL
+
+T = TypeVar('T')
+
+
+class IRepository(ABC, Generic[T]):
+
+    @abstractmethod
+    def find_by_id(self, id: str) -> Optional[T]:
+        ...
+
+
+class IShortenedURLRepository(IRepository):
+
+    @abstractmethod
+    def find_by_original_address(self, addr: str) -> Optional[ShortenedURL]:
+        ...
+
+    @abstractmethod
+    def insert(self, url: ShortenedURL):
+        ...
