612fd530c16098e32f1f4656cbaadc27e6f2ec57
Andrea Marino <am.marinoandrea@gmail.com>
2022-04-04 14:05:45 +0200

    Defined business entities

---
+++ services/url-shortener/url_shortener/entities/shortened_url.py
@@ -0,0 +1,51 @@
+import re
+from dataclasses import asdict, dataclass
+
+import cuid
+from url_shortener.errors import ValidationError
+
+from .unique_entity import UniqueEntity, make_unique_entity
+
+
+@dataclass
+class ShortenedURL(UniqueEntity):
+    """
+    Represents a shortened URL record.
+    """
+    original_address: str
+    short_id: str
+
+
+def make_url(data: dict) -> ShortenedURL:
+    """
+    Returns a `ShortenedURL` populated with the values in `data`
+    or throws an exception if the data does not contain valid fields.
+
+    Parameters
+    ----------
+    data: `dict` Dictionary containing entity data
+
+    Returns
+    -------
+    `ShortenedURL`
+    """
+    entity = make_unique_entity(data)
+
+    if (
+        'original_address' not in data or
+        type(data['original_address']) != str or
+        re.fullmatch(
+            r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+            data['original_address']
+        ) is None
+    ):
+        raise ValidationError("A ShortenedURL must contain a valid address.")
+
+    if 'short_id' in data and type(data['short_id']) != str:
+        raise ValidationError("A ShortenedURL must contain a valid short id.")
+
+    return ShortenedURL(
+        **asdict(entity),
+        original_address=data['original_address'],
+        short_id=data['short_id'] or cuid.slug()
+    )
---
+++ services/url-shortener/url_shortener/entities/unique_entity.py
@@ -0,0 +1,50 @@
+import time
+import uuid
+from dataclasses import dataclass
+
+from url_shortener.errors import ValidationError
+
+
+@dataclass
+class UniqueEntity:
+    """
+    Represents a base class for business entities that can be stored 
+    in the data layer via a repository manager.
+    """
+    id: str
+    created_at: int
+    updated_at: int
+
+
+def make_unique_entity(data: dict) -> UniqueEntity:
+    """
+    Returns a `UniqueEntity` populated with the values in `data`
+    or throws an exception if the data does not contain valid fields.
+
+    Parameters
+    ----------
+    data: `dict` Dictionary containing entity data
+
+    Returns
+    -------
+    `UniqueEntity`
+    """
+    time_ms = int(time.time() * 1000)
+
+    # unique entity fields are very often generated on entity creation
+    # so there is no need to pass them unless you are rebuilding the
+    # entity from the data layer.
+    if 'id' in data and type(data['id']) != str:
+        raise ValidationError("A UniqueEntity must contain a valid id.")
+
+    if 'created_at' in data and (type(data['created_at']) != int or data['created_at'] > time_ms):
+        raise ValidationError("A UniqueEntity must contain a valid timestamp for created_at.")
+
+    if 'updated_at' in data and (type(data['updated_at']) != int or data['updated_at'] > time_ms):
+        raise ValidationError("A UniqueEntity must contain a valid timestamp for updated_at.")
+
+    return UniqueEntity(
+        id=data['id'] or uuid.uuid4(),
+        created_at=data['created_at'] or time_ms,
+        updated_at=data['updated_at'] or time_ms
+    )
