d770098907d89ce74fd40d7211c092aaf0baa393
Andrea Marino <am.marinoandrea@gmail.com>
2022-04-09 19:27:59 +0200

    Core routes refactor to implement GET and DELETE for single urls

--- services/url-shortener/url_shortener/server/core.py
+++ services/url-shortener/url_shortener/server/core.py
@@ -1,50 +1,56 @@
-
-import os
-from dataclasses import asdict
-
-from flask import Blueprint, jsonify, request,make_response
-from url_shortener.use_cases import create_url
-from url_shortener.use_cases import get_url,get_ids
+from flask import Blueprint, abort, jsonify, redirect, request
+from url_shortener.errors import ValidationError
+from url_shortener.use_cases import create_url, delete_url, get_ids, get_url
 
 blueprint = Blueprint('core', __name__, url_prefix='/')
 
 
-@blueprint.route("/", methods=['POST', 'GET','PUT','DELETE'])
+@blueprint.errorhandler(ValidationError)
+def handle_bad_request(e):
+    return str(e), 400
+
+
+@blueprint.route("/", methods=['POST', 'GET'])
 def route_urls():
-
+    """
+    This route handles CRUD operations on the entire 
+    collection of the resource 'shortened url'.
+    """
     if request.method == 'POST':
-        url = create_url(request.json.get('url'))
-        if url != None:
-            output = jsonify({**asdict(url), "new_url": f"{os.getenv('DOMAIN_NAME','https://bit.g4')}/{url.short_id}"})
-            response = make_response(output, 201)
-        else:
-            response = make_response('Error', 400)      
-        return response
-    else:
-        if request.method == 'GET': 
-            short_id = (request.json.get('id'))
-            if short_id != None:
-                url = get_url(short_id)
-                output = jsonify({**asdict(url)})
-                response = make_response(output, 301)
-            else:
-                output = jsonify(Keys = get_ids())
-                response = make_response(output, 200)           
-            return  response
-        else:
-            if request.method == 'PUT':
-                short_id = (request.json.get('id'))
-                if short_id != None:
-                    url = get_url(short_id)
-                    output = jsonify({**asdict(url)})
-                    response = make_response(output, 200)
-                else:
-                    response = make_response('Error',400)
-                return  response
-            else:
-                if request.method == 'DELETE':
-                    response = make_response('Not Found',404)
-                    return response
+        # validate user request
+        original_address = request.json.get('url')
+        if original_address is None:
+            raise ValidationError(
+                "The body of the request must contain a 'url' field.")
+        # create a shortened url
+        url = create_url(original_address)
+        # return a json representation of the entity
+        return jsonify(url), 201
+
+    if request.method == 'GET':
+        output = jsonify(get_ids())
+        return output, 200
 
 
-        
+@blueprint.route("/<string:id>", methods=['GET', 'PUT', 'DELETE'])
+def route_url(id: str):
+    """
+    This route handles CRUD operations on a single instance
+    of the resource 'shortened url' uniquely identified by
+    the :id url parameter.
+    """
+    if request.method == 'GET':
+        url = get_url(id)
+        if url is None:
+            abort(404)
+        return redirect(url.original_address)
+
+    if request.method == 'DELETE':
+        try:
+            delete_url(id)
+        except ValidationError:
+            abort(404)
+        return "", 204
+
+    if request.method == 'PUT':
+        raise NotImplementedError()
