8f448e5e1e8e30802a1dcbb1c072b2e2dc205fa5
Andrea Marino <am.marinoandrea@gmail.com>
2022-04-10 11:21:12 +0200

    Fixed linter warnings

--- services/front-service/src/components/HeaderComponent.jsx
+++ services/front-service/src/components/HeaderComponent.jsx
@@ -1,25 +1,15 @@
-import React, { Component } from 'react'
+import React from "react";
 
-class HeaderComponent extends Component {
-    constructor(props) {
-        super(props)
+const HeaderComponent = () => (
+  <header>
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div>
+        <a href="/" className="navbar-brand">
+          Url Shortener Management
+        </a>
+      </div>
+    </nav>
+  </header>
+);
 
-        this.state = {
-                 
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <header>
-                    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a className="navbar-brand">Url Shortener Management</a></div>
-                    </nav>
-                </header>
-            </div>
-        )
-    }
-}
-
-export default HeaderComponent
+export default HeaderComponent;
