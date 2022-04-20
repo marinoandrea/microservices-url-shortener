374ad7ddf300923bb0f65eb4bb6bb449e74d3b0d
Andrea Marino <am.marinoandrea@gmail.com>
2022-04-20 17:20:01 +0200

    Added logging callback for Flask servers
    This allows for HTTP requests logging even in the WSGI server.
    
    NOTE: we may want to restrict this only to development environments.

--- services/auth-service/auth_service/server/__init__.py
+++ services/auth-service/auth_service/server/__init__.py
@@ -1,5 +1,9 @@
 
-from flask import Flask
+import logging
+
+from flask import Flask, Response
+from flask import logging as flask_logging
+from flask import request
 
 from .extensions import cors
 from .users import blueprint as users_blueprint
@@ -9,8 +13,16 @@ def create_app(config_file='./config.py'):
     app = Flask(__name__)
     app.config.from_pyfile(config_file)
 
+    flask_logging.default_handler.setFormatter(
+        logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s"))
+
     register_blueprints(app)
     register_extensions(app)
+
+    @app.after_request
+    def log_request_info(res: Response):
+        app.logger.info(f"{request.method} {request.path} -> {res.status}")
+        return res
 
     return app
 
