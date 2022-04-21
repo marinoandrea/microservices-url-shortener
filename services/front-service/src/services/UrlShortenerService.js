be007e868a7dfc3f7f6cfc1671966e3b5ea4c2f7
damon wang <wangjycode@gmail.com>
2022-04-21 16:29:46 +0200

    change shortener url

--- services/front-service/src/services/UrlShortenerService.js
+++ services/front-service/src/services/UrlShortenerService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const URL_SHORTENER_API_BASE_URL = "http://localhost:3000/urlshortener";
+export const URL_SHORTENER_API_BASE_URL = "/urlshortener";
 
 class UrlShortenerService {
   getUrlShortener() {
