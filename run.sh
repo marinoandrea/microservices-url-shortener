c15a5619a999a89a4cca432b4706259238781576
Andrea Marino <am.marinoandrea@gmail.com>
2022-05-09 08:57:21 +0200

    Removed `prod` version of docker compose
    As we run Helm in production, there is no need for this version of the docker compose script which is only meant for development.

--- run.sh
+++ run.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/sh
-
-MODE=$1
-FILE=docker-compose.dev.yaml
-
-if [ $MODE = "dev" ]; then
-    FILE=docker-compose.dev.yaml
-elif [ $MODE = "prod" ]; then
-    FILE=docker-compose.prod.yaml
-fi
-
 # these paths are relative to the auth-service root folder
 export RSA_PUBLIC_PATH=jwt_public.pem
 export RSA_PRIVATE_PATH=jwt_private.pem
 
-docker-compose -f $FILE up --remove-orphans
+docker-compose up --remove-orphans
