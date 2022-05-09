d454597b1d47c8f4594313d4b9b6ea5b7ce9e5d7
damon wang <wangjycode@gmail.com>
2022-05-09 09:58:52 +0200

    add environment variable

--- run.sh
+++ run.sh
@@ -2,5 +2,11 @@
 # these paths are relative to the auth-service root folder
 export RSA_PUBLIC_PATH=jwt_public.pem
 export RSA_PRIVATE_PATH=jwt_private.pem
+export AUTH_MONGODB_USER=test
+export AUTH_MONGODB_PASS=test
+export AUTH_MONGODB_NAME=test
+export SHORTENER_MONGODB_USER=test
+export SHORTENER_MONGODB_PASS=test
+export SHORTENER_MONGODB_NAME=test
 
 docker-compose up --remove-orphans
